bd23240abb277058008b7166551b110e29b61e22
erickveil <erick@erickveil.net>
2017-11-29 10:39:30 -0800

    Added tables to generate a random adventure

--- alltables.cpp
+++ alltables.cpp
@@ -271,6 +271,237 @@ QString AllTables::dungeonType()
     return table.getRollTableEntry();
 }
 
+QString AllTables::dungeonLocation()
+{
+    RandomTable table;
+
+    table.addEntry("A building in a city", 5);
+    table.addEntry("Catacombs or sewers beneath a city", 4);
+    table.addEntry("Beneath a farmhouse", 4);
+    table.addEntry("Beneath a graveyard", 4);
+    table.addEntry("Beneath a ruined castle", 6);
+    table.addEntry("Beneath a ruined city", 4);
+    table.addEntry("Beneath a temple", 4);
+    table.addEntry("In a chasm", 4);
+    table.addEntry("In a cliff face", 4);
+    table.addEntry("In a desert", 4);
+    table.addEntry("In a forest", 4);
+    table.addEntry("In a glacier", 3);
+    table.addEntry("In a gorge", 3);
+    table.addEntry("In a jungle", 4);
+    table.addEntry("In a mountain pass", 4);
+    table.addEntry("In a swamp", 4);
+    table.addEntry("Beneath or ont top of a mesa", 4);
+    table.addEntry("In sea caves", 3);
+    table.addEntry("In several connected mesas", 4);
+    table.addEntry("On a mountain peak", 4);
+    table.addEntry("On a promontary", 4);
+    table.addEntry("On an island", 4);
+    table.addEntry("Underwater", 4);
+
+    RandomTable exoticLocation;
+    exoticLocation.addEntry("Among the branches of a tree");
+    exoticLocation.addEntry("Around a geyser");
+    exoticLocation.addEntry("Behind a waterfall");
+    exoticLocation.addEntry("Burried in an avalanche");
+    exoticLocation.addEntry("Burried in a sandstorm");
+    exoticLocation.addEntry("Burried in volcanic ash");
+    exoticLocation.addEntry("Castle or structure sunken in a swamp");
+    exoticLocation.addEntry("Castle or structure at the bottom of a sinkhole");
+    exoticLocation.addEntry("Floating on the sea");
+    exoticLocation.addEntry("In a meteorite");
+    exoticLocation.addEntry("On a demiplane or in a pocket dimension");
+    exoticLocation.addEntry("In an area devestated by a magical catastrophe");
+    exoticLocation.addEntry("On a cloud");
+    exoticLocation.addEntry("In the Feywild");
+    exoticLocation.addEntry("In the Shadowfell");
+    exoticLocation.addEntry("On an island in an underground sea");
+    exoticLocation.addEntry("In a volcano");
+    exoticLocation.addEntry("On the back of a gargantuan living creature");
+    exoticLocation.addEntry("Sealed inside a magical dome of force");
+    exoticLocation.addEntry("Inside a Mordenkaienen's magnificent mansion");
+    exoticLocation.addEntry("On an earth mote");
+    exoticLocation.addEntry("In a tiny, shrunken world");
+    exoticLocation.addEntry("Inside someone's body (requires shrinking)");
+
+    table.addEntry(exoticLocation.getRollTableEntry(), 5);
+
+
+    return table.getRollTableEntry();
+}
+
+QString AllTables::dungeonCreator()
+{
+
+    RandomTable table;
+
+    table.addEntry("Beholder");
+
+    RandomTable cult;
+    cult.addEntry("Demon worshippers");
+    cult.addEntry("Devil worshippers");
+    cult.addEntry("Elemental Air cult", 2);
+    cult.addEntry("Elemental Eargh cult", 2);
+    cult.addEntry("Elemental Fire cult", 2);
+    cult.addEntry("Elemental Water cult", 2);
+    cult.addEntry("Worshippers of an evil deity", 5);
+    cult.addEntry("Worshippers of a good deity", 2);
+    cult.addEntry("Worshippers of a neutral deity", 3);
+
+    table.addEntry("Cult or religious group: " + cult.getRollTableEntry(), 3);
+    table.addEntry("Dwarves", 4);
+    table.addEntry("Elves/drow");
+    table.addEntry("Giants");
+    table.addEntry("Hobgoblins");
+
+    RandomTable alignment;
+    alignment.addEntry("Lawful good");
+    alignment.addEntry("Neutral good");
+    alignment.addEntry("Chaotic good");
+    alignment.addEntry("Lawful neutral");
+    alignment.addEntry("Neutral");
+    alignment.addEntry("Chaotic neutral");
+    alignment.addEntry("Lawful evil");
+    alignment.addEntry("Neutral evil");
+    alignment.addEntry("Chaotic evil");
+
+    RandomTable npcClass;
+    npcClass.addEntry("Barbarian");
+    npcClass.addEntry("Bard");
+    npcClass.addEntry("Cleric");
+    npcClass.addEntry("Druid");
+    npcClass.addEntry("Fighter");
+    npcClass.addEntry("Monk");
+    npcClass.addEntry("Paladin");
+    npcClass.addEntry("Ranger");
+    npcClass.addEntry("Rogue");
+    npcClass.addEntry("Sorcerer");
+    npcClass.addEntry("Warlock");
+    npcClass.addEntry("Wizard");
+
+    table.addEntry("Humans: " + alignment.getRollTableEntry() + ", "
+                   + npcClass.getRollTableEntry(), 4);
+    table.addEntry("Kuo-toa");
+    table.addEntry("Lich");
+    table.addEntry("Mind flayers");
+    table.addEntry("Yuan-ti");
+    table.addEntry("Natural caverns (no creator)");
+
+    return table.getRollTableEntry();
+}
+
+QString AllTables::dungeonHistory()
+{
+    RandomTable table;
+
+    table.addEntry("Abandoned by creators", 3);
+    table.addEntry("Abandoned due to plague", 1);
+    table.addEntry("Conquered by invaders", 4);
+    table.addEntry("Creators destroyed by attacking raiders", 2);
+    table.addEntry("Creators destroyed by discovery made within the site", 1);
+    table.addEntry("Creators destroyed by internal conflict", 1);
+    table.addEntry("Creators destroyed by magical catastorphe");
+    table.addEntry("Creators destroyed by natural disaster", 2);
+    table.addEntry("Location cursed by the gods and shunned");
+    table.addEntry("Original creator still in control", 2);
+    table.addEntry("Overrun by planar creatures");
+    table.addEntry("Site of a great miracle");
+
+    return table.getRollTableEntry();
+}
+
+QString AllTables::adventureHook()
+{
+    RandomTable table;
+
+    table.addEntry("Town Crier");
+    table.addEntry("Billboard");
+    table.addEntry("Mysterious stranger approaches party");
+    table.addEntry("Messenger boy");
+    table.addEntry("Ad in paper");
+    table.addEntry("Overheard rumor");
+    table.addEntry("Wild eyed person looking for help");
+    table.addEntry("Press gang");
+    table.addEntry("Commotion outside");
+    table.addEntry("Drafted by nearby guard or authorities");
+    table.addEntry("Recruiter for an organization");
+    table.addEntry("Nearby cry for help");
+    table.addEntry("Wealthy patron recognizes the party as adventurous");
+    table.addEntry("Find a hidden note");
+    table.addEntry("Someone acting suspicious");
+    table.addEntry("Secret code in rot13. Investigation check reveals the code method.");
+    table.addEntry("Failed attempt to rob PCs and robber turns out to be desperate and in need of help");
+    table.addEntry("One of the PCs professions is needed for a task specific to it");
+    table.addEntry("an acquaintance of the party seeks them out for aid");
+    table.addEntry("a wealthy noble is searching all the usual spots for an adventuring party to hire");
+    table.addEntry("an old kook that nobody believes tells a wild tale of adventure to be had.");
+    table.addEntry("a troublesome agent challenges or attacks the party, bearing the adventure hook on his body");
+    table.addEntry("A suspicious person is making a drop, not noticing he is being watched, he removes a loose brick and places a small scroll inside and leaves. A little while later, another person arrives and checks the wrong loose brick. Scratching his head and a little irritated, he leaves. The scroll contains the hook or a lead to the hook.");
+    table.addEntry("An inscription on an item, wall, or piece of furniture. May start the quest directly, or direct the party to someone who does.");
+
+    return table.getRollTableEntry();
+}
+
+QString AllTables::generateRandomAdventure(int tier)
+{
+    QString desc;
+    desc = "HOOK: " + adventureHook() + "\n";
+    QString type = dungeonType();
+    desc += "TYPE: " + type + "\n";
+    desc += "LOCATION: " + dungeonLocation() + "\n";
+    desc += "CREATOR: " + dungeonCreator() + "\n";
+    desc += "HISTORY: " + dungeonHistory() + "\n";
+    int layout = roll(1,10);
+    desc += "Layout #: " + QString::number(layout) + "\n";
+    desc += "\n";
+    int numRooms = (layout <= 3) ? 4 : 5;
+
+    // generate rooms
+    for (int roomNumber = 1; roomNumber <= numRooms; ++roomNumber) {
+        desc += "ROOM " + QString::number(roomNumber) + ":\n";
+        if (type == "General") {
+            desc += roller.generateGeneralRoom(tier);
+        }
+        else if (type == "Death Trap") {
+            desc += roller.generateDeathTrapRoom(tier);
+        }
+        else if (type == "Lair") {
+            desc += roller.generateLairRoom(tier);
+        }
+        else if (type == "Maze") {
+            desc += roller.generateMazeRoomType(tier);
+        }
+        else if (type == "Planar Gate") {
+            desc += roller.generatePlanarGateRoom(tier);
+        }
+        else if (type == "Stronghold") {
+            desc += roller.generateStrongholdRoom(tier);
+        }
+        else if (type == "Temple") {
+            desc += roller.generateTempleRoom(tier);
+        }
+        else if (type == "Tomb") {
+            desc += roller.generateToomb(tier);
+        }
+        else if (type == "Treasure Vault") {
+            desc += roller.generateTreasureVault(tier);
+        }
+        else {
+            desc = "Unknown dungeon type.";
+        }
+
+        desc += "\n";
+    }
+
+    // generate halls
+    for (int hall=0; hall < 7; ++hall) {
+        char hallId = hall + 97;
+        desc += "HALL: " + QString::fromLocal8Bit(hallId) + "\n";
+        desc += generateHallwayContents(tier);
+    }
+
+}
+
 QString AllTables::generalDungeonRoomType()
 {
     RandomTable table;
@@ -1387,6 +1618,7 @@ QString AllTables::lighting()
 
     return table.getRollTableEntry();
 }
+
 
 QString AllTables::generateActivity()
 {
